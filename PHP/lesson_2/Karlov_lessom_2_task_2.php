b7188844211553742bc40e069f473429e35e8d7e
Aleksey Karlov <all@xaker.ru>
2022-02-13 15:20:34 +0300

    php_2

---
+++ PHP/lesson_2/Karlov_lessom_2_task_2.php
@@ -0,0 +1,24 @@
+<?php
+
+
+// switch($a){
+//     case 1:
+//     case 2:
+//     echo ‘Hello world’;
+//     break;
+//     case 3:
+//     echo ‘Hi world’;
+//     break;
+//     default:
+//     echo ‘Goodbye world’;
+//    } 
+
+if ($a == 1) {
+    return;
+} else if ($a == 2) {
+    echo "Hello, world!";
+} else if ($a == 3) {
+    echo "Hi world";
+} else {
+    echo "Goodbye world";
+}
