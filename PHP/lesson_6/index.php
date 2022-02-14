93898cf0d79c88b571e10bfc3bba8d064e7247fe
Aleksey Karlov <all@xaker.ru>
2022-02-14 22:11:53 +0300

    lesson_6

---
+++ PHP/lesson_6/index.php
@@ -0,0 +1,15 @@
+<?php
+   $ot = 0;
+   $not = 0;
+      if ($_POST["q1"] == "a"){$ot++;} else {$not++;}
+      if ($_POST["q2"] == "a"){$ot++;} else {$not++;}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+    <p>Правильных ответов:</p>
+    <p>Неправильных ответов:</p>
+</body> 
+</html>
